1f13bd065e0d491e3f05df3120eaa00ecb70e3fe
Fane <fane247@gmail.com>
2017-07-13 16:39:57 +0100

    added check for no image set

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,33 @@ class RestaurantsController < ApplicationController
 	def index
 		
 		@restaurants = Restaurant.all
+
+		@restaurants.map do |restaurant|
+
+			restaurant.image_url = restaurant.image_url.nil? ? "/assets/unknown.png" : restaurant.image_url
+
+		end
+
+
 		
 	end
+
+	def show
+
+		@restaurant = Restaurant.find(params[:id])
+
+	end
+
+
+
+
+
+	 private
+    # Never trust parameters from the scary internet, only allow the white list through.
+        def character_params
+          params.require(:character).permit(:name, :age, :voice_actor, :first_appearance, :job)
+        end
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713132520) do
+ActiveRecord::Schema.define(version: 20170713153411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170713132520) do
     t.string "review"
     t.boolean "vegan_friendly"
     t.boolean "vegitarian_friendly"
+    t.string "image_url"
   end
 
 end
